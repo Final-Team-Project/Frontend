84481bc2317a98c2b9cb900f5daf19ac6b78e10a
rnaqpddl123 <83867157+rnaqpddl123@users.noreply.github.com>
2021-11-04 17:26:39 +0900

    errorcheck
    error_check

--- webapp/models.py
+++ webapp/models.py
@@ -3,21 +3,17 @@ from sqlalchemy.orm import relationship, backref
 from webapp.init_db import Base
 
 class User(Base):
-    __tablename__ = 'jin'
-    print('__tabelname__' '==== 접속성공')
-    id = Column(Integer, primary_key=True)
+    __tablename__ = 'JIN'
+    id = Column(Integer, primary_key=True)  
     email = Column(String, unique=True)
-    passwd = Column(String)
+    passwd = Column(Integer)
     nickname = Column(String)
 
     def __init__(self, email=None, passwd=None, nickname='손님', makeSha=False):
         self.email = email
-        if makeSha:
-            self.passwd = func.sha2(passwd, 256)
-        else:
-            self.passwd = passwd
+        self.passwd = passwd
         self.nickname = nickname
-    print('정보입력 성공')
 
     def __repr__(self):
-        return 'User %s, %r, %r, %r' % (self.id, self.email, self.passwd, self.nickname)
+        return 'User %s, %r, %r,%r' % (self.id, self.email, self.passwd,self.nickname)
+    
